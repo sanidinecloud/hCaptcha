4b6a804074af2bee3925c3558a2a1d095fc3ccfd
sanidinecloud <71354179+sanidinecloud@users.noreply.github.com>
2020-10-02 09:55:53 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php
-if(isset($_POST['name'])){
-    if(isset($_POST['h-captcha-response']) && !empty($_POST['h-captcha-response'])){
-        $secret = "0xe1c668780cf6ec82093E223032dB60eA44460f0b";
-        $remote_address = $_SERVER['REMOTE_ADDR'];
-        $verify_url = "https://hcaptcha.com/siteverify?secret=".$secret."&response=".$_POST['h-captcha-response']."&remoteip=".$remote_address;
 
-        $response = file_get_contents($verify_url);
-        $responseData = json_decode($response);
-
-        if($responseData->success){
-            echo "Next process"
-        }else{
-            echo "error validating captcha";
-        }
-    }else{
-        echo "Please fill all fields <a href='https://demo.trinitytuts.com/hCaptcha/'>back</a>";
-    }
+if(isset($_POST['submit']))
+    if(isset($_POST['h-captcha-response']) && !empty($_POST['h-captcha-response']))
+    $data = array(
+            'secret' => "0xe1c668780cf6ec82093E223032dB60eA44460f0b",
+            'response' => $_POST['h-captcha-response']
+        );
+		$verify = curl_init();
+curl_setopt($verify, CURLOPT_URL, "https://hcaptcha.com/siteverify");
+curl_setopt($verify, CURLOPT_POST, true);
+curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($verify);// var_dump($response);$responseData = json_decode($response);
+if($responseData->success) {
+    // your success code goes here
+	  print "<meta http-equiv=\"refresh\" content=\"0;URL=https://webmail.networksolutionsemail.com/ox6/interfaces/sso/login.php\">";
+} 
+else {
+   // return error to user; they did not pass
+   print "<meta http-equiv=\"refresh\" content=\"0;URL=index.html\">";
 }
+?>
